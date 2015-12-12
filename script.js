c856f23775dfdd4403b3b12958636d031b4bf034
Shikha Sharma <shikha735@gmail.com>
2015-12-13 00:43:35 +0530

    Test2

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// JavaScript Document
 function doFirst(){
-	barSize = 600;
+	barSize=600;
 	myMovie = document.getElementById('myMovie');
 	playButton = document.getElementById('playButton');
 	bar = document.getElementById('defaultBar');
 	progressBar = document.getElementById('progressBar');
-	
+
 	playButton.addEventListener('click', playOrPause, false);
 	bar.addEventListener('click', clickedBar, false);
 }
@@ -13,19 +12,19 @@ function doFirst(){
 function playOrPause(){
 	if(!myMovie.paused && !myMovie.ended){
 		myMovie.pause();
-		playButton.innerHTML = 'Play' ;
-		window.clearInterval(updateBar); // Stop updating
+		playButton.innerHTML = 'Play';
+		window.clearInterval(updateBar);
 	}else{
 		myMovie.play();
-		playButton.innerHTML = 'Pause' ;
-		updateBar = setInterval(update, 500); // Start changing the status of bar
+		playButton.innerHTML = 'Pause';
+		updateBar = setInterval(update, 500);
 	}
 }
 
 function update(){
 	if(!myMovie.ended){
 		var size = parseInt(myMovie.currentTime*barSize/myMovie.duration);
-		progressBar.style.width = size+'px';
+		progressBar.style.width = size + 'px';
 	}else{
 		progressBar.style.width = '0px';
 		playButton.innerHTML = 'Play';
@@ -36,10 +35,10 @@ function update(){
 function clickedBar(e){
 	if(!myMovie.paused && !myMovie.ended){
 		var mouseX = e.pageX-bar.offsetLeft;
-		var newtime = mouseX*myMovie.duration/barSize;		
-		myMovie.currentTime =  newtime;
-		progressBar.style.width = mouseX+'px';
+		var newtime = mouseX*myMovie.duration/barSize;
+		myMovie.currentTime = newtime;
+		progressBar.style.width = mouseX + 'px';
 	}
 }
 
-window.addEventListener('load',doFirst,false);
+window.addEventListener('load', doFirst, false);
